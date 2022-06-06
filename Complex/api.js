cdb04952fe897bd2eb53accd0058d19ddacf6473
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-06 18:49:09 +0800

    constructor

---
+++ Complex/api.js
@@ -0,0 +1,47 @@
+/* https://github.com/Skyhigh173/javascript-tool/blob/main/Complex/ComplexNum.js */
+/* Complex Calculator api v1.0 */
+
+export class ComplexNumer {
+    constructor() {
+        /* Math Const */
+        
+        /**
+        * @type {number} retrun math const PI in complex form. Num : 3.141592...
+        * @public
+        */
+        this.PI;
+        
+        /**
+        * @type {number} retrun math const E in complex form. Num : 2.718281828...
+        * @public
+        */
+        this.E;
+        
+        /**
+        * @type {number} retrun math const I in complex form. Num : sqrt(-1)
+        * @public
+        */
+        this.I;
+        
+        /**
+        * @type {number} retrun math const sqrt(2) in complex form. Num : 1.414213...
+        * @public
+        */
+        this.SQRT2;
+        
+        /**
+        * @type {number} retrun math const sqrt(3) in complex form. Num : 1.73205...
+        * @public
+        */
+        this.SQRT3;
+        
+        /**
+        * @type {number} retrun math const omega in complex form. Num : 0.56714...
+        * @public
+        */
+        this.OMEGA;
+    }
+        
+        
+        
+        

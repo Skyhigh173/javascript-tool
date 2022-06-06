a0bb280acb15e52b634978426c463c38672c8cec
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-06 09:07:43 +0800

    Create ComplexNum-v1.js

---
+++ Complex/ComplexNum-v1.js
@@ -0,0 +1,30 @@
+/*
+Complex num v1
+
+supported calc:
++
+-
+*
+/
+sin()
+cos()
+log()
+ln()
+tan()
+^
+
+usage : 
+var a = new ComplexNum(1,2); // 1+2i
+
+*/
+
+
+
+function ComplexNum(real, img) {
+  this.r = real;
+  this.i = img;
+  this.value = real + "+" + img + "i";
+}
+function Plus(a, b) {
+  return new ComplexNum((a.r + b.r), (a.i + b.i));
+}

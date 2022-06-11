55f7ef3bcc5f0e0a444fd8e664b90ada9fe3d587
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-11 18:03:01 +0800

    Update A-Plus-B.js

--- Challenge/A-Plus-B.js
+++ Challenge/A-Plus-B.js
@@ -2,10 +2,11 @@ function SolverV1 (a, b){
   // a and b must be a string
   let k = Math.max(a.length, b.length);
   let result = "";
-  let next = 0;
-  for (let i = k, i > 0, i--){
-    let an = a.charAt(i), bn = b.charAt(i);
-    let sum = an + bn + next;
+  let next=0,an=0,bn=0,sum=0;
+  for (let i = k; i > 0; i--){
+    an = a.charAt(i);
+    bn = b.charAt(i);
+    sum = an + bn + next;
     next = 0;
     if (sum >= 10) { sum -= 10; next += 1; };
     result = sum.toString() + result;

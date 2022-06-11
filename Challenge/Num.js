4ff199b96bf3db55e859b0bf7648504320c68eae
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-11 17:07:37 +0800

    Update Num.js

--- Challenge/Num.js
+++ Challenge/Num.js
@@ -2,7 +2,7 @@ function SolverV1 (inp) {
   let chars = "0689abdgopq", val = "11211111111";
   let c = 0;
   for (let i=0; i < inp.length; i++) {
-    if (chars.includes(inp.charAt(i))) c += val.charAt(char.indexOf(inp.charAt(i)));
+    if (chars.includes(inp.charAt(i))) c += Number(val.charAt(chars.indexOf(inp.charAt(i))));
   }
   return c;
 }

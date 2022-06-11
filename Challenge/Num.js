b91afbf5fbaa689ba2d3b5630f3dd0ba5c4782bb
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-11 17:16:57 +0800

    Update Num.js

--- Challenge/Num.js
+++ Challenge/Num.js
@@ -1,5 +1,5 @@
 function SolverV1 (inp) {
-  let chars = "0689abdgopq", val = "11211111111";
+  let chars = "0689abdgeopqBDOPQR", val = "112111111111211111";
   let c = 0;
   for (let i=0; i < inp.length; i++) {
     if (chars.includes(inp.charAt(i))) c += Number(val.charAt(chars.indexOf(inp.charAt(i))));

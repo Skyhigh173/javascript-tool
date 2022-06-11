ba85c48d450b57d83a5b22fdb27fab1f51e8a516
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-11 18:04:01 +0800

    Update A-Plus-B.js

--- Challenge/A-Plus-B.js
+++ Challenge/A-Plus-B.js
@@ -4,12 +4,13 @@ function SolverV1 (a, b){
   let result = "";
   let next=0,an=0,bn=0,sum=0;
   for (let i = k; i > 0; i--){
-    an = a.charAt(i);
-    bn = b.charAt(i);
+    an = a.charAt(i-1);
+    bn = b.charAt(i-1);
     sum = an + bn + next;
     next = 0;
     if (sum >= 10) { sum -= 10; next += 1; };
     result = sum.toString() + result;
   }
-  return result;
+  if (next >= 1) return next.toString() + result;
+  else return result;
 }

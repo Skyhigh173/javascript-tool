ab98d1433b5ef1bc77aacd1db9e2449baa4f10aa
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-29 11:21:57 +0800

    Update numbers.js

--- Others/Math/numbers.js
+++ Others/Math/numbers.js
@@ -6,12 +6,16 @@ export const math {
     if (type == 0) console.log("Math error");
   }
   
+  ilogb: (arg) => {
+    return Math.floor(Math.log(arg) / Math.log(2));
+  }
   
   log10: (M) => {
     let log2_hi = 3.01029995663611771306e-01;
     let log2_lo = 3.69423907715893078616e-13;
     if (M == 0) return NaN;
     if (M <= 0) return NaN;
-    let k = 0;
+    let n = math.ilogb(M);
+    if (n < 0) n -= 1;
   }
 }

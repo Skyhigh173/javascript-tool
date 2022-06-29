f8e3d69e59139b7252c43f31c1d2ce3d15c0092d
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-29 13:30:14 +0800

    /* hi */

--- Others/Math/numbers.js
+++ Others/Math/numbers.js
@@ -32,6 +32,7 @@ export const math {
   }
     
   pow: (b, e) => {
+    /* 10^(abs(b) * e) */
     let ans = 10 ** (math.log10(Math.abs(b)) * e);
     if (b <= 0 && math.mod(e, 2) > 1) ans = -ans;
     return ans;
@@ -39,17 +40,18 @@ export const math {
   
   log10: (M) => {
     let getLength = (txt) => {
+      /* similar to log but no float */
       if (txt >= 1) return Number(Math.floor(txt).toString().length-1);
       else return Number(Math.floor(1/txt).toString().length-1);
     }
-
     
     let txt = getLength(M);
-    
     let result = txt.toString() + ".";
     let Mn = (M / (10 ** txt)) ** 10;
     
     for (let i = 0; i < 50; i++) {
+      /* iterate by using (M_n / 10^(A_n))^10 */
+      /* where M_n is last calculation ans, A_n is total digit-1 of M_n */
       txt = getLength(Mn);
       Mn = (Mn / (10 ** txt)) ** 10;
       result += txt.toString();
@@ -58,6 +60,7 @@ export const math {
   }
   
   logN: (base, M) => {
+    /* log(M) / log(base) */
     return math.log10(M) / math.log10(base);
   }
 }

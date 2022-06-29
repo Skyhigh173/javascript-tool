33be8219e5ce42bb0ff4c3e4c2b03cee524ffcdc
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-06-29 13:25:50 +0800

    Update numbers.js

--- Others/Math/numbers.js
+++ Others/Math/numbers.js
@@ -1,9 +1,40 @@
 /* an api of Math */
 
+/*
+* mod
+* pow
+* log (n)
+* ln()
+* sin
+* cos
+* tan
+* sinh
+* cosh
+* tanh
+* sin-1 (asin) (arcsin)
+* cos-1 (acos) (arccos)
+* tan-1 (atan) (arctan)
+* 
+*
+*
+*/
+
 export const math {
   
   err: (type) => {
     if (type == 0) console.log("Math error");
+  }
+  
+  mod: (a, b) => {
+    /* a mod b */
+    let i = Math.floor(a / b);
+    return (a / b - i) * a + i;
+  }
+    
+  pow: (b, e) => {
+    let ans = 10 ** (math.log10(Math.abs(b)) * e);
+    if (b <= 0 && math.mod(e, 2) > 1) ans = -ans;
+    return ans;
   }
   
   log10: (M) => {
